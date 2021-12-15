feb527fb6b9c271cd4c8f43749b2aa8229e8f176
Jσєℓ Bσвαη [Oғғlιɴe] <95572329+JoelBobanOffline@users.noreply.github.com>
2021-12-15 08:22:52 +0530

    Create cancel_broadcast.py

---
+++ plugins/cancel_broadcast.py
@@ -0,0 +1,23 @@
+from pyrogram import Client, filters
+
+from config import Config
+
+
+@Client.on_callback_query(
+    filters.create(lambda _, __, query: query.data.startswith("cncl_bdct"))
+    & filters.user(Config.AUTH_USERS)
+)
+async def cncl_broadcast_(c, cb):
+
+    _, broadcast_id = cb.data.split("+")
+
+    if not c.broadcast_ids.get(broadcast_id):
+        await cb.answer(
+            text=f"No active broadcast with id {broadcast_id}", show_alert=True
+        )
+        return
+
+    broadcast_handler = c.broadcast_ids[broadcast_id]
+    broadcast_handler.cancel()
+
+    await cb.answer(text="Broadcast will be canceled soon.", show_alert=True)
