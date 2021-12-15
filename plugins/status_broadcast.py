9cec239790dbc1a9e8b6043560ca6935aac72fd9
Jσєℓ Bσвαη [Oғғlιɴe] <95572329+JoelBobanOffline@users.noreply.github.com>
2021-12-15 08:21:39 +0530

    Create status_broadcast.py

---
+++ plugins/status_broadcast.py
@@ -0,0 +1,28 @@
+from pyrogram import Client, filters
+
+from config import Config
+
+
+@Client.on_callback_query(
+    filters.create(lambda _, __, query: query.data.startswith("sts_bdct"))
+    & filters.user(Config.AUTH_USERS)
+)
+async def sts_broadcast_(c, cb):
+
+    _, broadcast_id = cb.data.split("+")
+
+    if not c.broadcast_ids.get(broadcast_id):
+        await cb.answer(
+            text=f"No active broadcast with id {broadcast_id}", show_alert=True
+        )
+        return
+
+    sts_txt = ""
+    broadcast_handler = c.broadcast_ids[broadcast_id]
+    broadcast_progress = broadcast_handler.get_progress()
+    for key, value in broadcast_progress.items():
+        sts_txt += f"{key} = {value}\n"
+
+    await cb.answer(
+        text=f"Broadcast Status for {broadcast_id}\n\n{sts_txt}", show_alert=True
+    )
