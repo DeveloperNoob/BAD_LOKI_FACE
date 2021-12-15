f3f169c32999ac8a96fde5952e741c521923fc0a
BLACK DEVIL TG <95171491+BLACK-DEVIL-TG@users.noreply.github.com>
2021-12-15 08:47:29 +0530

    😊

--- plugins/commands.py
+++ plugins/commands.py
@@ -243,7 +243,7 @@ async def help(client, message):
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("How to Deploy?", url="https://youtu.be/hkmc3e7U7R4"),
+                    InlineKeyboardButton("Dev", url="https://t.me/BLACK_DEVIL_TG"),
                     InlineKeyboardButton("About Me", callback_data="about_data")
                 ],
                 [
@@ -265,7 +265,7 @@ async def about(client, message):
             [
                 [
                     InlineKeyboardButton(
-                        "SOURCE CODE", url="https://github.com/TroJanzHEX/Unlimited-Filter-Bot")
+                        "SOURCE CODE", url="https://github.com/JOHN-WICK-TG/Unlimited-Filter-Bot")
                 ],
                 [
                     InlineKeyboardButton("BACK", callback_data="help_data"),
